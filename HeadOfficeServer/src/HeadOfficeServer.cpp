91e870eb15991001477830e36355bf00d4d15302
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-09 13:44:41 +0000

    Creates ServerConnectionControl object and calls run() to start server
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@30881 ae35fe06-0421-0410-bee3-800000000000

---
+++ HeadOfficeServer/src/HeadOfficeServer.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HeadOfficeServer.cpp
+// Author      : Tom Frampton
+// Version     :
+// Copyright   : Copyright Tom Frampton 2013
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
