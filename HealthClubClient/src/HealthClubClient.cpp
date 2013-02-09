0103e0f1e3961840ada592ef932bdc2d140bacb0
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-09 16:09:59 +0000

    Creates a ClientConnectionControl object and calls connectToHeadOffice().
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@30891 ae35fe06-0421-0410-bee3-800000000000

---
+++ HealthClubClient/src/HealthClubClient.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HealthClubClient.cpp
+// Author      : Tom Frampton
+// Version     :
+// Copyright   : Copyright Tom Frampton 2013
+// Description : Represents a local health club that will connect to the
+// 				 company's head office server
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
