e47dbafce5316a66cbf5fc73f45fc2f0792a2272
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-11 12:06:01 +0000

    Added proper comments for each file and function. Moved setting up the address data into it's own private method.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@30990 ae35fe06-0421-0410-bee3-800000000000

--- HealthClubClient/src/ClientConnectionControl.cpp
+++ HealthClubClient/src/ClientConnectionControl.cpp
@@ -1,63 +1,80 @@
 /*
- * ClientConnectionControl.cpp
- *
- *  Created on: 9 Feb 2013
- *      Author: Tom
+ * @file 	 ClientConnectionControl.cpp
+ * @author	 Tom Frampton
+ * @date 	 2013-02-09
+ * @version  1.0
+ * Copyright 2013 Tom Frampton
  */
 
 #include "ClientConnectionControl.h"
 
 namespace Tom_F {
 
-ClientConnectionControl::ClientConnectionControl() {
-	// TODO Auto-generated constructor stub
-
-}
-
-ClientConnectionControl::~ClientConnectionControl() {
-	// TODO Auto-generated destructor stub
-}
-
-int ClientConnectionControl::connectToHeadOffice(){
-	// Start WinSock
-	if(initWinSock() != 0){
-		// throw winsock startup excepti
-		exit(1);
+	ClientConnectionControl::ClientConnectionControl()
+	{
+		// TODO Auto-generated constructor stub
 	}
 
-	addrLen = sizeof(addr);
-
-	addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(CONN_PORT);
-
-	connectSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(connect(connectSock, (SOCKADDR*)&addr, addrLen) != 0){
-		// throw connection exception
-		return 1;
+	ClientConnectionControl::~ClientConnectionControl()
+	{
+		// TODO Auto-generated destructor stub
 	}
-	else{
-		// cout connected
-		return 0;
+
+	void ClientConnectionControl::connectToHeadOffice()
+	{
+		// Start WinSock
+		if(initWinSock() != 0){
+			// throw winsock startup exception
+		}
+
+		SOCKADDR_IN addressData = initAddressData();
+		int addressLength = sizeof(addressData);
+
+		if(connect(connectionSocket, (SOCKADDR*)&addressData, addressLength) != 0)
+		{
+			// throw connection exception
+		}
+		else
+		{
+			// cout connected
+		}
 	}
-}
 
-void ClientConnectionControl::disconnect(){
-	closesocket(connectSock);
-	WSACleanup();
-}
+	void ClientConnectionControl::disconnect()
+	{
+		closesocket(connectionSocket);
+		WSACleanup();
+	}
 
-void ClientConnectionControl::sendMessage(char* message){
-	int bufferSize = sizeof(message) / sizeof(char);
-	char buffer[bufferSize];
-	ZeroMemory(buffer, bufferSize);
-	send(connectSock, buffer, bufferSize, 0);
-}
+	void ClientConnectionControl::sendMessage(char* message)
+	{
+		int bufferSize = sizeof(message) / sizeof(char);
+		char buffer[bufferSize];
+		ZeroMemory(buffer, bufferSize);
+		send(connectionSocket, buffer, bufferSize, 0);
+	}
 
-int ClientConnectionControl::initWinSock(){
-	WSAData wsa;
-	WORD dllVersion = MAKEWORD(2,1);
-	return (WSAStartup(dllVersion, &wsa));
-}
+	int ClientConnectionControl::initWinSock()
+	{
+		WSAData wsa;
+		WORD dllVersion = MAKEWORD(2,1);
+		return (WSAStartup(dllVersion, &wsa));
+	}
+
+	SOCKADDR_IN ClientConnectionControl::initAddressData()
+	{
+		SOCKADDR_IN addressData;
+		int addressLength = sizeof(addressData);
+
+		listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		connectionSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+		// Set values for the address data struct
+		addressData.sin_addr.s_addr = htonl(INADDR_ANY);
+		addressData.sin_family = AF_INET;
+		addressData.sin_port = htons(CONN_PORT);
+
+		return addressData;
+	}
 
 }
--- HealthClubClient/src/ClientConnectionControl.h
+++ HealthClubClient/src/ClientConnectionControl.h
@@ -1,8 +1,13 @@
 /*
- * ClientConnectionControl.h
- *
- *  Created on: 9 Feb 2013
- *      Author: Tom
+ * @file 	 ClientConnectionControl.h
+ * @author	 Tom Frampton
+ * @date 	 2013-02-09
+ * @version  1.0
+ * Copyright 2013 Tom Frampton
+ */
+
+/**
+ * This class is responsible for establishing and managing the connection to the server.
  */
 
 #ifndef CLIENTCONNECTIONCONTROL_H_
@@ -11,26 +16,30 @@
 #include <WinSock2.h> // For sockets
 #include <Windows.h>
 
-#define IP_ADDRESS "127.0.0.1"
-#define CONN_PORT 8000
+#define IP_ADDRESS "127.0.0.1" // The IP address that the server resides on
+#define CONN_PORT 8000 // The port that is used to connect to the server
 
-namespace Tom_F {
+namespace Tom_F
+{
+	class ClientConnectionControl
+	{
+	public:
+		ClientConnectionControl();
+		virtual ~ClientConnectionControl();
 
-class ClientConnectionControl {
-public:
-	ClientConnectionControl();
-	virtual ~ClientConnectionControl();
-	int connectToHeadOffice();
-	void disconnect();
-	void sendMessage(char* message);
+		void connectToHeadOffice();
+		void disconnect();
 
-private:
-	int initWinSock();
+		void sendMessage(char* message);
 
-	SOCKET connectSock;
-	SOCKADDR_IN addr;
-	int addrLen;
-};
+	private:
+		int initWinSock();
+		SOCKADDR_IN initAddressData();
+
+		SOCKET listeningSocket;
+		SOCKET connectionSocket;
+	};
 
 } /* namespace Tom_F */
+
 #endif /* CLIENTCONNECTIONCONTROL_H_ */
