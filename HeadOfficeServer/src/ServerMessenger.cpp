c3375e684b71dd8a628e7cf2980b968d18980b2c
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-14 12:43:12 +0000

    Added this class to translate char* messages sent over the network to the appropriate message calls on the server side.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@31093 ae35fe06-0421-0410-bee3-800000000000

---
+++ HeadOfficeServer/src/ServerMessenger.cpp
@@ -0,0 +1,24 @@
+/*
+ * @file 	 ServerConnectionControl.h
+ * @author	 Tom Frampton
+ * @date 	 2013-02-14
+ * @version  1.0
+ * Copyright 2013 Tom Frampton
+ */
+
+#include "ServerMessenger.h"
+
+namespace Tom_F {
+
+	ServerMessenger::ServerMessenger()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	ServerMessenger::~ServerMessenger()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace Tom_F */
