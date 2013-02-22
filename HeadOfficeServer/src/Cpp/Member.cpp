e7b74493a6171146a3676aee2cc8abf97f36a038
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-22 14:27:45 +0000

    Represents a member at the club. Added boost serialization functionality.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@31502 ae35fe06-0421-0410-bee3-800000000000

---
+++ HeadOfficeServer/src/Cpp/Member.cpp
@@ -0,0 +1,32 @@
+/*
+ * Member.cpp
+ *
+ *  Created on: 18 Feb 2013
+ *      Author: Tom
+ */
+
+#include "Member.h"
+
+namespace Tom_F
+{
+	Member::Member() : membershipNo(-1)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	Member::Member(int memberNo) : membershipNo(memberNo)
+	{
+
+	}
+
+	Member::~Member()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+	int Member::getMembershipNo() const
+	{
+		return membershipNo;
+	}
+
+} /* namespace Tom_F */
---
+++ HeadOfficeServer/src/Headers/Member.h
@@ -0,0 +1,40 @@
+/*
+ * Member.h
+ *
+ *  Created on: 18 Feb 2013
+ *      Author: Tom
+ */
+
+#ifndef MEMBER_H_
+#define MEMBER_H_
+
+#include <boost/serialization/base_object.hpp>
+#include "Person.h"
+
+namespace Tom_F
+{
+	class Member : public Person
+	{
+		public:
+			Member();
+			Member(int membershipNo);
+			virtual ~Member();
+
+			int getMembershipNo() const;
+
+		private:
+			const int membershipNo;
+
+			friend class boost::serialization::access;
+			template<class Archive>
+			void serialize(Archive& ar, const unsigned int version)
+			{
+				ar & boost::serialization::base_object<Person>(*this);
+				ar & membershipNo;
+			}
+
+		};
+
+} /* namespace Tom_F */
+
+#endif /* MEMBER_H_ */
