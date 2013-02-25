d5773f50067ad0e25ed0819c57127d4f928a426b
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-02-25 23:06:04 +0000

    Start of iteration 3 - Training now involves the member being paired with a trainer. This class represents and controls the queue of trainers.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@31627 ae35fe06-0421-0410-bee3-800000000000

---
+++ HealthClubClient/src/Cpp/TrainerQueue.cpp
@@ -0,0 +1,21 @@
+/*
+ * TrainerQueue.cpp
+ *
+ *  Created on: 25 Feb 2013
+ *      Author: Tom
+ */
+
+#include "TrainerQueue.h"
+
+namespace Tom_F {
+
+TrainerQueue::TrainerQueue() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TrainerQueue::~TrainerQueue() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Tom_F */
---
+++ HealthClubClient/src/Headers/TrainerQueue.h
@@ -0,0 +1,20 @@
+/*
+ * TrainerQueue.h
+ *
+ *  Created on: 25 Feb 2013
+ *      Author: Tom
+ */
+
+#ifndef TRAINERQUEUE_H_
+#define TRAINERQUEUE_H_
+
+namespace Tom_F
+{
+	class TrainerQueue {
+	public:
+		TrainerQueue();
+		virtual ~TrainerQueue();
+	};
+
+} /* namespace Tom_F */
+#endif /* TRAINERQUEUE_H_ */
