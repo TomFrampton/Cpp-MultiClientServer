c4e5e8f8729970c6d11e1ef16087c088cc0a8aed
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-03-04 22:48:33 +0000

    When a new connection is found this class now creates a Connection Handler object to handle the running of that socket on it's own thread.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@32012 ae35fe06-0421-0410-bee3-800000000000

---
+++ HeadOfficeServer/src/Headers/ServerConnectionControl.h
@@ -0,0 +1,73 @@
+/*
+ * @file 	 ServerConnectionControl.h
+ * @author	 Tom Frampton
+ * @date 	 2013-02-09
+ * @version  1.0
+ * Copyright 2013 Tom Frampton
+ */
+
+/**
+ * This class is responsible for establishing and managing the connection with the server.
+ */
+
+#ifndef SERVERCONNECTIONCONTROL_H_
+#define SERVERCONNECTIONCONTROL_H_
+
+#include <WinSock2.h> // For sockets
+#include <Windows.h> // For Sleep()
+#include <pthread.h> // For POSIX threads
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+#include "ConnectionHandler.h"
+#include "HeadOfficeAdmin.h"
+
+using std::ostream; // To overload the << operator
+using std::stringstream;
+using std::vector;
+using boost::shared_ptr;
+
+#define CONN_PORT 8066 // Server will connect on port 8000
+#define BUF_SIZE 4096 // The server will accept packet sized char*
+#define MAX_CONN 64 // A maximum of 64 connections at once
+
+namespace Tom_F
+{
+	class ConnectionData;
+
+	class ServerConnectionControl
+	{
+	public:
+		ServerConnectionControl();
+		ServerConnectionControl(const HeadOfficeAdmin* admin);
+		virtual ~ServerConnectionControl();
+
+		void run();
+
+		stringstream toString();
+		friend ostream& operator<<(ostream&, const ServerConnectionControl&);
+
+	private:
+		int initWinSock();
+		void initAddressData();
+		void listenForConnections();
+		SOCKET* findAvailableSocket();
+		void processNewConnection();
+
+		SOCKADDR_IN addressData;
+		SOCKET listeningSocket;
+		SOCKET connectionSocket;
+		const HeadOfficeAdmin* const admin;
+
+		SOCKET* connections; // To hold all connection sockets
+		int connCount; // Number of connected clients
+		vector<shared_ptr<ConnectionHandler> > handlers;
+
+		pthread_mutex_t findConnectionLock;
+	};
+
+} /* namespace Tom_F */
+
+#endif /* SERVERCONNECTIONCONTROL_H_ */
