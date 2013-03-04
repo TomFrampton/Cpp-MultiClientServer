dff046b89940bad587664d34d67254d56136ee05
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-03-04 22:53:06 +0000

    Added POSIX semaphores to maintain thread safety between training threads. Threads will sleep if there are no trainers available and be woken up when one becomes available.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@32016 ae35fe06-0421-0410-bee3-800000000000

--- HealthClubClient/src/Cpp/TrainerQueue.cpp
+++ HealthClubClient/src/Cpp/TrainerQueue.cpp
@@ -5,17 +5,66 @@
  *      Author: Tom
  */
 
+#include <iostream>
 #include "TrainerQueue.h"
+#include <sstream>
+
+using namespace std;
 
 namespace Tom_F {
 
-TrainerQueue::TrainerQueue() {
-	// TODO Auto-generated constructor stub
+	TrainerQueue::TrainerQueue()
+	{
+		trainerQueue.push(Trainer());
+		initSemaphores(1);
+	}
 
-}
+	TrainerQueue::TrainerQueue(vector<Trainer> trainersToUse)
+	{
+		vector<Trainer>::iterator it = trainersToUse.begin();
+		while(it != trainersToUse.end())
+		{
+			trainerQueue.push(*it);
+			it++;
+		}
+		initSemaphores(trainersToUse.size());
+	}
 
-TrainerQueue::~TrainerQueue() {
-	// TODO Auto-generated destructor stub
-}
+	TrainerQueue::~TrainerQueue() {
+		// TODO Auto-generated destructor stub
+	}
+
+	Trainer TrainerQueue::getTrainer()
+	{
+		sem_wait(&available);
+		sem_wait(&mutex);
+
+		Trainer nextTrainer = trainerQueue.front();
+		trainerQueue.pop();
+
+		sem_post(&notAvailable);
+		sem_post(&mutex);
+
+		return nextTrainer;
+	}
+
+	void TrainerQueue::returnTrainer(Trainer trainer)
+	{
+
+		sem_wait(&notAvailable);
+		sem_wait(&mutex);
+
+		trainerQueue.push(trainer);
+
+		sem_post(&available);
+		sem_post(&mutex);
+	}
+
+	void TrainerQueue::initSemaphores(int numberOfTrainers)
+	{
+		sem_init(&mutex, 0, 1);
+		sem_init(&available, 0, numberOfTrainers);
+		sem_init(&notAvailable, 0, 0);
+	}
 
 } /* namespace Tom_F */
--- HealthClubClient/src/Headers/TrainerQueue.h
+++ HealthClubClient/src/Headers/TrainerQueue.h
@@ -8,12 +8,34 @@
 #ifndef TRAINERQUEUE_H_
 #define TRAINERQUEUE_H_
 
-namespace Tom_F
-{
-	class TrainerQueue {
+#include "Trainer.h"
+#include <pthread.h>
+#include <Semaphore.h>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+namespace Tom_F {
+
+	class TrainerQueue
+	{
 	public:
 		TrainerQueue();
+		TrainerQueue(vector<Trainer> trainersToUse);
 		virtual ~TrainerQueue();
+
+		Trainer getTrainer();
+		void returnTrainer(Trainer trainer);
+
+	private:
+		sem_t mutex;
+		sem_t available;
+		sem_t notAvailable;
+
+		queue<Trainer, deque<Trainer> > trainerQueue;
+
+		void initSemaphores(int numberOfTrainers);
 	};
 
 } /* namespace Tom_F */
