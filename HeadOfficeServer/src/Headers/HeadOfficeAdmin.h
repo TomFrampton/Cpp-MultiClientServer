ce45f988cdcdf37ef037e53fd6066be4bda613b4
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-03-07 23:03:51 +0000

    Renamed from HeadOfficeAdministration.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@32369 ae35fe06-0421-0410-bee3-800000000000

---
+++ HeadOfficeServer/src/Headers/HeadOfficeAdmin.h
@@ -0,0 +1,44 @@
+/*
+ * HeadOfficeAdmin.h
+ *
+ *  Created on: 20 Feb 2013
+ *      Author: Tom
+ */
+
+#ifndef HEADOFFICEADMIN_H_
+#define HEADOFFICEADMIN_H_
+
+#include "Guest.h"
+#include "HealthData.h"
+#include "Person.h"
+#include "TrainingMeeting.h"
+#include "SimulationData.h"
+#include "DBControl.h"
+
+namespace Tom_F
+{
+	class UserInterfaceIF;
+
+	class HeadOfficeAdmin
+	{
+	public:
+		HeadOfficeAdmin();
+		HeadOfficeAdmin(DBControl* db, UserInterfaceIF* ui);
+		virtual ~HeadOfficeAdmin();
+
+		int registerHealthClub() const;
+		int hireEmployee(const Person& toHire, string position, int healthClubID) const;
+		int enrolMember(const Guest& newMember, int healthClubID) const;
+		int logHealthData(const HealthData& data) const;
+		HealthData getLastHealthLog(int membershipID) const;
+		int logTrainingMeeting(const TrainingMeeting& meeting) const;
+		SimulationData getSimulationData(int healthClubID) const;
+
+	private:
+		DBControl* db;
+		UserInterfaceIF* userInterface;
+	};
+
+} /* namespace Tom_F */
+
+#endif /* HEADOFFICEADMIN_H_ */
