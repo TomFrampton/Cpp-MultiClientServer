efdcb4458957b77d2f8eb06414015235a3f7edf1
u1171639 <u1171639@ae35fe06-0421-0410-bee3-800000000000>
2013-03-08 11:12:11 +0000

    Added a catch clause to catch ConnectionException.
    git-svn-id: http://hermes.hud.ac.uk/svn/compeng/students/u1171639/Assignment@32465 ae35fe06-0421-0410-bee3-800000000000

--- HealthClubClient/src/Application/Cpp/HealthClubClient.cpp
+++ HealthClubClient/src/Application/Cpp/HealthClubClient.cpp
@@ -1,11 +1,16 @@
 //============================================================================
 // Name        : HealthClubClient.cpp
+
 // Author      : Tom Frampton
 // Version     :
 // Copyright   : Copyright Tom Frampton 2013
 // Description : Represents a local health club that will connect to the
 // 				 company's head office server
 //============================================================================
+
+#include "CommunicationException.h"
+#include "ConnectionException.h"
+
 #include "ConsoleUI.h"
 #include "SimulationControl.h"
 #include "PoissonArrivalGenerator.h"
@@ -16,9 +21,23 @@ using namespace Tom_F;
 
 int main() {
 
-	ConsoleUI ui;
-	SimulationControl simulation(&ui);
-	ui.startSimulation(simulation);
+	ConsoleUI userInterface;
 
+	try
+	{
+		SimulationControl simulation(&userInterface);
+		userInterface.startSimulation(simulation);
+	}
+	catch(Exception::CommunicationException& exc) // Error with communicating with server
+	{
+		userInterface.printOutput(exc.getExplanation()); // Print explanation
+	}
+	catch(Exception::ConnectionException& exc) // Error with the client's connection
+	{
+		userInterface.printOutput(exc.getExplanation()); // Print explanation
+	}
+	// Print fatal error message
+	userInterface.printOutput("\nFatal server error. Server must close");
+	getchar(); // Wait for user input
 	return 0;
 }
